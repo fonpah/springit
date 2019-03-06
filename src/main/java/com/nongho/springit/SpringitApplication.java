088d8c2771a3969ca473bd7cc1b89192737d6463
fon-pah <fonpah@yahoo.fr>
2019-03-06 15:25:56 +0100

    making use of lombok

--- src/main/java/com/nongho/springit/SpringitApplication.java
+++ src/main/java/com/nongho/springit/SpringitApplication.java
@@ -1,14 +1,34 @@
 package com.nongho.springit;
 
+import com.nongho.springit.config.SpringitProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
 
+    @Autowired
+    private SpringitProperties springitProperties;
+    private static Logger logger = LoggerFactory.getLogger(SpringitApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
-        System.out.println("Welcome to springit!");
+    }
+    @Bean
+    CommandLineRunner runner(){
+        return arg -> {
+            System.out.println("Our message is : " + springitProperties.getWelcomeMsg());
+            logger.warn("warning");
+            logger.error("Error");
+            logger.info("Info");
+        };
     }
 
 }
+
